344d363b7259ffec0ae54deddff7a75fe3e6f521
Richard Koranteng <richard@rkkoranteng.com>
2020-04-03 10:08:33 -0400

    Create mount.filesystem.sh

---
+++ mount.filesystem.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# script: mount_fs.sh
+# author: Richard K @ www.rkkoranteng.com
+# desc: format and mount linux fs
+# last modified: 11/28/2017
+
+if [ $# -ne 2 ]
+then
+ echo "Usage: $0 [device] [mount point]"
+ exit
+fi
+
+devname=$1
+mntname=$2
+
+if [ ! -d /${mntname} ]
+then
+ mkdir /${mntname}
+fi
+
+mkfs.ext4 /dev/${devname}
+echo "/dev/${devname} /${mntname} ext4 defaults,noatime 1 1" >> /etc/fstab
+mount /${mntname}
+
+df -h
